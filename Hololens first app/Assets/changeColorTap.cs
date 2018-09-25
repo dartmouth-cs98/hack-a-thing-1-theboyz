edf77b3c3fc943dd5733b7c93da6231fb09401d2
Oliver McTammany <oliver.m.mctammany.19@dartmouth.edu>
2018-09-25 16:53:51 -0400

    adding colored cube and bouncing ball for hololens to repo

---
+++ Hololens first app/Assets/changeColorTap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public class changeColorTap : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+
+    }
+}
